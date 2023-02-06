fb43dac09041d94f4ebd81b624f0f8ae5cbd0e97
Kateryna Solodilova <kiivikat@gmail.com>
2023-02-06 17:11:18 +0100

    Finishes task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,16 @@ import { nanoid } from 'nanoid';
 import toast, { Toaster } from 'react-hot-toast';
 
 const INITIAL_STATE = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
   filter: '',
 };
+
+const INITIAL_CONTACTS = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 const TITLES = {
   form: 'Phonebook',
@@ -25,6 +27,21 @@ const TITLES = {
 
 export class App extends Component {
   state = { ...INITIAL_STATE };
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    } else {
+      this.setState({ contacts: INITIAL_CONTACTS });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     if (this.state.contacts.find(contact => contact.name === name)) {
